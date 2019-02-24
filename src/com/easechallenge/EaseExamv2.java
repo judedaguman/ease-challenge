87fed65421cf127a109384851d19ae89fbc6f0ad
judedaguman <37826831+judedaguman@users.noreply.github.com>
2019-02-24 16:24:51 +0800

    Add files via upload
    Added File Reader

--- src/com/easechallenge/EaseExamv2.java
+++ src/com/easechallenge/EaseExamv2.java
@@ -1,6 +1,11 @@
 package com.easechallenge;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
@@ -15,14 +20,48 @@ public class EaseExamv2{
 	public static void main (String[] args) {
 		
 		//test 4x4 example
-		List<Integer [] > mapArray = new ArrayList<Integer []>();
-		//int longestPath = 0;
-        Integer[][] map2d = { {4, 8, 7, 3}, {2, 5, 9, 3}, {6, 3, 2, 5}, {4,4,1,6} };
 		
-        for (int i= 0;  i< map2d.length; i++) {
-        		mapArray.add(map2d[i]);
+		//int longestPath = 0;
+		List<Integer [] > mapArray = new ArrayList<Integer []>();
+        //Integer[][] map2d = { {4, 8, 7, 3}, {2, 5, 9, 3}, {6, 3, 2, 5}, {4,4,1,6} };
+		
+	
+        FileReader fr;
+		try {
+			fr = new FileReader(new File("").getAbsolutePath()+"\\src\\com\\easechallenge\\map.txt");
+			BufferedReader br = new BufferedReader(fr);
+	        String line = br.readLine();
+	       
+	        String[] arrayLength = line.split(" ", 2);
+	        Integer rowLength = Integer.parseInt(arrayLength[0]);
+	        Integer colLength = Integer.parseInt(arrayLength[1]);
+	        line = null;
+	        Integer[][] arr = new Integer[rowLength][colLength];
+	        	for (int i = 0; i < rowLength; i++) {
+	                String[] st = br.readLine().trim().split(" ");
+	                for (int j = 0;j < colLength; j++) {
+	                	arr[i][j]  = Integer.parseInt(st[j]) ;
+	                }
+	           }
+	        	
+	          for (int i= 0;  i< rowLength; i++) {
+        		mapArray.add(arr[i]);
 			
 		}
+	        
+	        br.close();
+	        
+	        
+		} catch (FileNotFoundException e) {
+			System.out.println("Error File Not Found");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("File Read Error");
+			e.printStackTrace();
+		}
+        
+        
+        
 		//MountainMap testMap = new MountainMap();
         //display(mapArray,numRows,numCols);
 		Runner resultSet = new Runner();
